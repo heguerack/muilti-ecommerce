638ebaaa41974f6fc26a70cfd63e85ef58bb40ce
Frank <heguer76@gmail.com>
2025-06-07 08:30:21 -0600

    02:Customization

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Input } from '@/components/ui/input'
+import { Progress } from '@/components/ui/progress'
+import { Textarea } from '@/components/ui/textarea'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <div className='flex flex-col gap-y-4 p-4'>
+        <div className=''>
+          <Button variant={'elevated'}>I am a button</Button>
+        </div>
+        <div className=''>
+          <Input placeholder='I am an input'></Input>
+        </div>
+        <div className=''>
+          <Progress value={50}></Progress>
+        </div>
+        <div className=''>
+          <Textarea placeholder='Im a text area'></Textarea>
+        </div>
+        <div className=''>
+          <Checkbox />
+        </div>
+      </div>
+    </>
+  )
+}
